46fe9b4ac38a30ce769ce6bd5edcb23c9ad79412
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-11-03 22:54:09 -0500

    navigate implementation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
-
-  // get the navigate function
+  const navigate = useNavigate();
 
   const goBack = () => {
+    navigate(-1)
     // imperatively redirect back
-    
-  }
+  };
 
   const goForward = () => {
+    navigate(+1)
     // imperatively redirect forward
-    
-  }
+  };
 
   return (
     <footer>
